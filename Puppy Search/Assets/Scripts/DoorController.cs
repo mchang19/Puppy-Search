e610d94a1111109a2c3d61fc84b3af61bf8a77ff
mchang19 <46766756+mchang19@users.noreply.github.com>
2020-12-31 18:43:11 -0800

    Skybox, doors, fencing
    - DoorController script, triggers, animators added
    - Garden fencing and initial boundary fencing added
    - Skybox added

---
+++ Puppy Search/Assets/Scripts/DoorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    private Animator doorAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    //Detects if player is within the box collider range of the door
+    private void OnTriggerEnter(Collider other)
+    {
+        doorAnimator.SetBool("DoorInRange", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAnimator.SetBool("DoorInRange", false);
+    }
+}
