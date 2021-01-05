cb864495a8fd6a8ca91d322fef40f798d89bbfcd
mchang19 <46766756+mchang19@users.noreply.github.com>
2021-01-04 18:23:02 -0800

    2nd clue implementation
    Set up second (?) clue to be triggered when chicken feed plane and dog food plane are visible, need to finish being able to replant carrot and making footprints appear

---
+++ Puppy Search/Assets/Scripts/TriggerNextClue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerNextClue : MonoBehaviour
+{
+    public GameObject prereqClue1; //Object that should have been made visible in order for next clue to appear
+    public GameObject prereqClue2;
+    public GameObject nextClue1; //Object to make visible/invisible to set up for next clue
+    public GameObject nextClue2;
+    public GameObject nextClue3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextClue1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prereqClue1.GetComponent<Renderer>().enabled == true && prereqClue2.GetComponent<Renderer>().enabled == true)
+        {
+            visCheck(nextClue1);
+            visCheck(nextClue2);
+            visCheck(nextClue3);
+        }
+    }
+
+    //Check if the nextClue(x) object should be made visible/invisible
+    private void visCheck(GameObject _visCheck)
+    {
+        if (_visCheck.CompareTag("MakeVisible"))
+        {
+            _visCheck.SetActive(true);
+        }
+        else
+        {
+            _visCheck.SetActive(false);
+        }
+    }
+}
